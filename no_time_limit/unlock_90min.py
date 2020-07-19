9b7bc902e407d010dfda0122cb4e63c5bf1de711
MIDORIBIN <aquarion123@gmail.com>
2020-07-19 14:10:01 +0000

    fix: クラス化

--- no_time_limit/unlock_90min.py
+++ no_time_limit/unlock_90min.py
@@ -11,57 +11,71 @@ import glob
 import sys
 import threading
 
+class Unlock90Min:
 
-def install_chromium():
-    cache = apt.Cache()
-    cache.update()
-    cache.open(None)
-    cache.commit()
+    @staticmethod
+    def install_chromium():
+        cache = apt.Cache()
+        cache.update()
+        cache.open(None)
+        cache.commit()
 
-    pkg = cache['chromium-chromedriver']
-    pkg.mark_install()
-    cache.commit()
+        pkg = cache['chromium-chromedriver']
+        pkg.mark_install()
+        cache.commit()
 
-def create_notebook_page(notebook_url: str, user_data_dir: str) -> webdriver.Chrome:
-    options = webdriver.ChromeOptions()
-    options.add_argument("--user-data-dir=" + user_data_dir)
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome("chromedriver", options=options)
-    driver.get(notebook_url)
+    @staticmethod
+    def create_notebook_page(notebook_url: str, user_data_dir: str) -> webdriver.Chrome:
+        options = webdriver.ChromeOptions()
+        options.add_argument("--user-data-dir=" + user_data_dir)
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Chrome("chromedriver", options=options)
+        driver.get(notebook_url)
 
-    return driver
+        return driver
 
-def save_screenshot(driver: webdriver.Chrome, path: str = 'screenshot.png') -> None:
-    original_size = driver.get_window_size()
-    driver.set_window_size(1080, 4000)
-    driver.find_element_by_tag_name('body').screenshot(path)
-    driver.set_window_size(original_size['width'], original_size['height'])
+    def __init__(self, notebook_url: str, user_data_dir: str):
+        self.driver = Unlock90Min.create_notebook_page(notebook_url, user_data_dir)
+        self.__duration_hour = 11
 
-def periodic_process(driver: webdriver.Chrome):
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,'connect'))).click()
-    path = '/content/drive/My Drive/ETTTS/screenshot.png'
-    save_screenshot(driver, path)
+    @property
+    def duration_hour(self):
+        return self.__duration_hour
 
-def end_process():
-    pass
+    @duration_hour.setter
+    def duration_hour(self, duration_hour):
+        self.__duration_hour = duration_hour
 
-def persistence_notebook(driver: webdriver.Chrome):
-    hour = 9
-    for i in range(hour * 6):
-        time.sleep(10 * 60)
-        periodic_process(driver)
-    end_process()
+    def save_screenshot(self, path: str = 'screenshot.png') -> None:
+        original_size = self.driver.get_window_size()
+        self.driver.set_window_size(1080, 4000)
+        self.driver.find_element_by_tag_name('body').screenshot(path)
+        self.driver.set_window_size(original_size['width'], original_size['height'])
 
-def start(notebook_url: str, user_data_dir: str):
-    print('start install chromium')
-    install_chromium()
-    print('end install chromium')
+    def periodic_process(self):
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.ID,'connect'))).click()
+        path = '/content/drive/My Drive/ETTTS/screenshot.png'
+        self.save_screenshot(path)
 
-    driver = create_notebook_page(notebook_url, user_data_dir)
+    def end_process(self):
+        pass
 
-    save_screenshot(driver, 'drive/My Drive/ETTTS/screenshot.png')
+    def persistence_notebook(self):
+        for i in range(self.__duration_hour * 6):
+            time.sleep(10 * 60)
+            self.periodic_process()
+        self.end_process()
+    
+    def start(self):
+        print('start install chromium')
+        Unlock90Min.install_chromium()
+        print('end install chromium')
 
-    print('start unlock 90 min')
-    threading.Thread(target=persistence_notebook, args=(driver, )).start()
+        time.sleep(10)
+        self.save_screenshot()
+
+        print('start unlock 90 mina')
+        threading.Thread(target=persistence_notebook, args=(driver, )).start()
+
